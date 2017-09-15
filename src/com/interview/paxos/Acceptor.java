89e20bb1f26f61ae6bb0b5ed7a0db6be507a2a6f
caisz <596630076@qq.com>
2017-09-15 17:21:02 +0800

    fisrt commit

--- src/com/interview/paxos/Acceptor.java
+++ src/com/interview/paxos/Acceptor.java
@@ -4,7 +4,11 @@ import com.interview.paxos.message.AcceptRequest;
 import com.interview.paxos.message.PrepareRequest;
 import com.interview.paxos.message.PrepareResponse;
 
+/**
+ *  提议接受者接口
+ */
 public interface Acceptor extends Component{
+
 
     public PrepareResponse prepare(PrepareRequest m);
 
--- src/com/interview/paxos/Component.java
+++ src/com/interview/paxos/Component.java
@@ -2,8 +2,11 @@ package com.interview.paxos;
 
 import com.interview.paxos.message.SourceDestination;
 
+
 public interface Component {
 
+
     void receiveMessage(SourceDestination sd, Message m);
+
     String getId();
 }
